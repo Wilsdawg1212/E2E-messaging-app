4a543080481527a3496ba235c05d9f91688cd7be
Wilsdawg1212 <145064244+Wilsdawg1212@users.noreply.github.com>
2024-12-29 20:20:32 -0500

    Server now recieves messages from clients.

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,17 +1,20 @@
 use tokio_tungstenite::connect_async;
+use futures_util::{StreamExt, SinkExt};
 use tokio::io::{self, AsyncBufReadExt};
-use futures_util::{SinkExt, StreamExt};
 
 #[tokio::main]
 async fn main() {
     let url = "ws://127.0.0.1:3030/ws";
-    let (mut socket, _) = connect_async(url).await.expect("Failed to connect");
+    let (socket, _) = connect_async(url).await.expect("Failed to connect");
 
     println!("Connected to the server!");
 
-    // Spawn a task to read incoming messages
+    // Split the WebSocket into writer and reader
+    let (mut writer, mut reader) = socket.split();
+
+    // Spawn a task to handle incoming messages
     tokio::spawn(async move {
-        while let Some(msg) = socket.next().await {
+        while let Some(msg) = reader.next().await {
             match msg {
                 Ok(msg) if msg.is_text() => {
                     println!("Received: {}", msg.to_text().unwrap());
@@ -25,10 +28,10 @@ async fn main() {
     let stdin = io::BufReader::new(io::stdin());
     let mut lines = stdin.lines();
 
-    while let Some(Ok(line)) = lines.next_line().await {
+    while let Ok(Some(line)) = lines.next_line().await {
         if line == "/quit" {
             break;
         }
-        socket.send(line.into()).await.expect("Failed to send message");
+        writer.send(line.into()).await.expect("Failed to send message");
     }
 }
